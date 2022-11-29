d2f263dd016177872b92630768b15363b90d983b
Raphitpt <43700748+Raphitpt@users.noreply.github.com>
2022-11-29 09:42:01 +0100

    Delete Envoyer les fichiers vers git.bat

--- Envoyer les fichiers vers git.bat
+++
@@ -1,7 +0,0 @@
-cd ./
-set HOME=%USERPROFILE%
-git checkout main
-git add .
-git commit -am "made changes"
-git push https://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@github.com/Raphitpt/SAE105_IUT.git
-pause
