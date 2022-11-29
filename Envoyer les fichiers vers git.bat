c32b0aa3e553137d6fc2e062a86eb55984258aa0
TIPHONET RAPHAEL <rtiphone@iut.local>
2022-11-29 09:19:41 +0100

    made changes

--- Envoyer les fichiers vers git.bat
+++ Envoyer les fichiers vers git.bat
@@ -1,7 +1,7 @@
 cd ./
 set HOME=%USERPROFILE%
-git checkout dev
+git checkout main
 git add .
 git commit -am "made changes"
-git push
+git push https://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@github.com/Raphitpt/SAE105_IUT.git
 pause
